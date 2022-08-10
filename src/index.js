af23089affc8d3d87f58af64da7a8bf7438ed56a
Ian <ian.wiedenman@gmail.com>
2022-08-10 16:23:19 -0700

    Added stringify

--- src/index.js
+++ src/index.js
@@ -160,6 +160,10 @@
     return isObject(o);
   };
 
+  Powertools.stringify =  function (obj, replacer, spaces, cycleReplacer) {
+    return JSON.stringify(obj, serializer(replacer, cycleReplacer), spaces);
+  };
+
   // Helpers
   function getKeys(obj, prefix) {
     var keys = Object.keys(obj);
@@ -176,6 +180,28 @@
 
   function isObject(o) {
     return Object.prototype.toString.call(o) === '[object Object]';
+  }
+
+  // https://github.com/moll/json-stringify-safe/blob/master/stringify.js
+  function serializer(replacer, cycleReplacer) {
+    var stack = [], keys = []
+
+    if (cycleReplacer == null) cycleReplacer = function(key, value) {
+      if (stack[0] === value) return "[Circular ~]"
+      return "[Circular ~." + keys.slice(0, stack.indexOf(value)).join(".") + "]"
+    }
+
+    return function(key, value) {
+      if (stack.length > 0) {
+        var thisPos = stack.indexOf(this)
+        ~thisPos ? stack.splice(thisPos + 1) : stack.push(this)
+        ~thisPos ? keys.splice(thisPos, Infinity, key) : keys.push(key)
+        if (~stack.indexOf(value)) value = cycleReplacer.call(this, key, value)
+      }
+      else stack.push(value)
+
+      return replacer == null ? value : replacer.call(this, key, value)
+    }
   }
 
   function forceString(input) {
